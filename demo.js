ee2331519c1cc4c29bdbb1c070ad21d7bac88b8f
SnapeRohan <indulkarrohan0@gmail.com>
2024-11-26 16:14:22 +0530

    5th commit

--- demo.js
+++ demo.js
@@ -5,10 +5,6 @@ console.log("Hello Hii")
 console.log("Hello Hii")
 console.log("Hello Hii")
 
-function add(){
-    console.log("Add method")
-}
-
 function sub(){
     console.log("sub method")
 }
